0db7f6a7b1e28940aa7e7b1d15c85b090b0599d9
Arthur Santos <arthursantos@Arthurs-MacBook-Pro.local>
2024-11-11 09:53:40 -0300

    chore: define initial scope

--- kubernetes/client.go
+++ kubernetes/client.go
@@ -2,57 +2,37 @@ package kubernetes
 
 import (
 	"flag"
-	"log"
-
-	// "k8s.io/client-go/kubernetes"
 	"path/filepath"
 
-	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
 
 type K8sClient struct {
 	client *kubernetes.Clientset
-	dyn    dynamic.Interface
 }
 
 func NewK8sClient() *K8sClient {
 	var kubeconfig *string
-	var config *rest.Config
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
 
-	config, err := rest.InClusterConfig()
-
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
-		if home := homedir.HomeDir(); home != "" {
-			kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "optional - absolute path to the kubeconfig file")
-		} else {
-			kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-		}
-		flag.Parse()
-
-		localConfig, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-
-		if err != nil {
-			panic(err.Error())
-		}
-		config = localConfig
+		panic(err.Error())
 	}
 
-	kubernetesClient, err := kubernetes.NewForConfig(config)
+	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		log.Fatalln(err)
-	}
-
-	dynamicClient, err := dynamic.NewForConfig(config)
-	if err != nil {
-		log.Fatalln(err)
+		panic(err.Error())
 	}
 
 	return &K8sClient{
-		client: kubernetesClient,
-		dyn:    dynamicClient,
+		client: clientset,
 	}
 }
--- kubernetes/deployments.go
+++ kubernetes/deployments.go
@@ -2,21 +2,14 @@ package kubernetes
 
 import (
 	"context"
-	"fmt"
 	"log"
-	"os"
-	"strings"
 
 	v1App "k8s.io/api/apps/v1"
 	v1Core "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/util/yaml"
 )
 
-func (k *K8sClient) ListAllDeployments(namespaces []v1Core.Namespace) []*v1App.DeploymentList {
+func (k *K8sClient) GetAllDeploymentsPerNamespaces(namespaces []v1Core.Namespace) []*v1App.DeploymentList {
 	deployApi := k.client.AppsV1()
 
 	var lists []*v1App.DeploymentList
@@ -34,7 +27,7 @@ func (k *K8sClient) ListAllDeployments(namespaces []v1Core.Namespace) []*v1App.D
 	return lists
 }
 
-func (k *K8sClient) ListDeploymentsPerNamespace(namespaces string) *v1App.DeploymentList {
+func (k *K8sClient) GetAllDeploymentsPerNamespace(namespaces string) *v1App.DeploymentList {
 	deployApi := k.client.AppsV1()
 
 	deploy := deployApi.Deployments(namespaces)
@@ -58,75 +51,4 @@ func (k *K8sClient) GetDeployment(namespace, deployment string) *v1App.Deploymen
 	}
 
 	return getDeploy
-}
-
-// ApplyYAML applies a Kubernetes manifest from a YAML file
-func (k *K8sClient) ApplyYAML(manifestPath string) error {
-	// Open the YAML file
-	file, err := os.Open(manifestPath)
-	if err != nil {
-		return fmt.Errorf("failed to open manifest file: %w", err)
-	}
-	defer file.Close()
-
-	// Create a YAML decoder for multi-document YAML
-	decoder := yaml.NewYAMLOrJSONDecoder(file, 4096)
-
-	for {
-		// Decode each document into an Unstructured object
-		obj := &unstructured.Unstructured{}
-		if err := decoder.Decode(obj); err != nil {
-			if err.Error() == "EOF" {
-				break // End of file
-			}
-			return fmt.Errorf("failed to decode YAML: %w", err)
-		}
-
-		// Get namespace from the object or set it to 'default'
-		namespace := obj.GetNamespace()
-		if namespace == "" {
-			namespace = "default"
-		}
-		obj.SetNamespace(namespace)
-
-		// Get the GroupVersionResource (GVR)
-		gvk := obj.GroupVersionKind()
-		gvr := schema.GroupVersionResource{
-			Group:    gvk.Group,
-			Version:  gvk.Version,
-			Resource: resourceName(gvk.Kind),
-		}
-
-		// Use the dynamic client to get the existing resource
-		resourceClient := k.dyn.Resource(gvr).Namespace(namespace)
-		existing, err := resourceClient.Get(context.TODO(), obj.GetName(), metav1.GetOptions{})
-
-		if errors.IsNotFound(err) {
-			// Create the resource if it doesn't exist
-			_, err = resourceClient.Create(context.TODO(), obj, metav1.CreateOptions{})
-			if err != nil {
-				return fmt.Errorf("failed to create resource: %w", err)
-			}
-			fmt.Printf("Resource %s created in namespace %s\n", obj.GetName(), namespace)
-		} else if err == nil {
-			// Set the resourceVersion from the existing object
-			obj.SetResourceVersion(existing.GetResourceVersion())
-
-			// Update the resource
-			_, err = resourceClient.Update(context.TODO(), obj, metav1.UpdateOptions{})
-			if err != nil {
-				return fmt.Errorf("failed to update resource: %w", err)
-			}
-			fmt.Printf("Resource %s updated in namespace %s\n", obj.GetName(), namespace)
-		} else {
-			return fmt.Errorf("failed to get resource: %w", err)
-		}
-	}
-
-	return nil
-}
-
-// resourceName converts a Kind to its plural, lowercase form
-func resourceName(kind string) string {
-	return strings.ToLower(kind) + "s" // Basic pluralization
 }
